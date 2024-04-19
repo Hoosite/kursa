a3676470653654b9fab88ce1cfa79d20520cbd4e
Hoosite <malandin.danil@mail.ru>
2024-04-19 10:00:02 +0500

    v2

--- page/schedule.py
+++ page/schedule.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from script.schedule_reader import read_schedule
 
 schedule = Blueprint('schedule', __name__, template_folder='templates')
@@ -7,12 +7,12 @@ schedule = Blueprint('schedule', __name__, template_folder='templates')
 def display_schedule():
     # Путь к файлу Excel с расписанием
     file_path = 'script/rasp.xlsx'
+    
+    # Получаем выбранную группу из параметров запроса
+    group_page = request.args.get('group', '1223_1227')
 
-    # Номер страницы с расписанием для нужной группы
-    group_page = '1223_1227'
-
-    # Чтение расписания из файла Excel
+    # Чтение расписания из файла Excel для выбранной группы
     schedule_group1, schedule_group2 = read_schedule(file_path, group_page)
 
     # Отображение расписания на веб-странице
-    return render_template('schedule.html', schedule_group1=schedule_group1, schedule_group2=schedule_group2)
+    return render_template('schedule.html', selected_group=group_page, schedule_group1=schedule_group1, schedule_group2=schedule_group2)
